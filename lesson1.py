e7b2b1f66db8990e0cd9e7288371e09e0c6767f2
Danchiiik <dcabatar@gmail.com>
2022-10-30 13:22:16 +0600

    week2

---
+++ lesson1.py
@@ -0,0 +1,196 @@
+
+# print('hello world')
+# print('hello')
+# - однострочный комментарий
+
+"""""
+многострочный комментарий
+"""""
+
+# print('5*5') - вывели раскдадку
+# print('5*5') # выведи 25 
+
+
+# print(1+1)
+# print(1-1)
+# print(1*1)
+
+
+# Переменная - это ссылка на ячейку памяти в которой хранится какая то информация. Это значит что переменные предназначены для хранения каких либо данный
+
+# s = 'hello world'
+# print(s)
+
+# age = 30
+# print (id( age ))
+# age2 = 32
+# print(id(32))
+
+# Как должны называться переменные:
+# age = 1
+# _x = 3
+# last_name = 'Snow'
+
+# Имена переменных не должны начинаться с цифр
+
+# 2age = 30 error
+# age2 = 30 not error
+
+# имя переменных не может содержать специальные символы @, #, $ и т. д.
+
+# имя переменных может содержать: буквы, нижнее подчеркивание, цифры в конце
+
+# нельзя использовать зарезервированные слова 
+# print = hello
+
+# age = 30
+# print(age)
+# age = 31
+# print(age)
+# age = 'hello'
+# print(age)
+
+# a = 5
+# print(type(a)) # type - показывает тип данных переменной
+# В переменной мы можем хранить разные значения делят на разные типы данных
+
+# Типы данных:
+#   int - целое число (3, 15, -100)
+#   float - число с плавающей запятой (4.3, 45.205) 
+#   complex комплексное число (10j, 3g)
+# 
+# String (строка)
+# List (лист)
+# Dictionary (словарь)
+# Tuple (кортеж)
+# Set (множества)
+# Boolean (Лог. тип данных)
+# NoneType (Неопределенное значение переменной )
+
+# int_= 100
+# int2 = -100
+# float_ = 100.0
+# complex_ = 3j
+
+# print(type(int_))
+# print(type(int2))
+# print(type(float_))
+# print(type(complex_))
+
+
+
+# name = input('write your name: ')
+# age = int(input('write your age: '))
+# print(name, age) 
+
+
+# name = input("Write your name: ")
+# age = 30
+# print('Hello, your name is', name, 
+# 'and your age', age )
+
+
+# s = '20'
+# print(type(s))
+# print(type(int(s)))
+# s = int(s)
+# print(type(s))
+
+#s = 'hello'
+#print(int(s))
+
+# print(2+2) # 4
+# print(2-2) # 0
+# print(2*2) #4
+# print(2/2) #1.0
+# print(2//2) #1 (целочисленное деление)
+# print(3%2) #1 остаток деления 
+# print(--2) #2
+# print(2**2) #4
+
+# from math import *
+# a = 5
+# b = 6
+# c = sqrt(a**2 + b**2)
+# f = hypot(a, b)
+# print(c)
+# print(f)
+
+# x, y, c = 2, 3, 4
+# print(x, y, c)
+
+# x, y = 5, 5
+# print(x+y) 
+# print(x-y) 
+# print(x*y) 
+# print(x/y) 
+# print(x//y) 
+# print(x%y)
+# print(x--y) 
+# print(x**y)
+
+# print('hello ' * 10)
+
+# x = -100 
+# print(abs(x)) # Модуль числа
+
+
+# age = 30
+# name = 'John'
+# print(id(age))
+# print(id(name))
+
+
+# x = 3 
+# y = 4
+# z = pow(x, y)
+# print(z)
+
+# print(pow(3, 2))
+# print(pow(3, 2, 2))
+
+# print(divmod(12, 4)
+# print(dir(math))
+# from math import pi as pee
+# x = 10
+# print(fff * x)
+
+# num1 = int(input('1st number:' ))
+# num2 = int(input('2nd number: ' ))
+# print(num1 + num2)
+
+# if True:
+#     print('John')
+# print('Hello')
+
+# x = 5
+# y = 3
+# operation = '+'
+# if operation == '+':
+#     print(x + y)
+
+#x, y= (input('write 1st number: ' )), (input('write 2nd number: ' ))
+#op = input('write operation: ')
+# if op == '+':
+#     print(x + y)
+# elif op == '-':
+#     print(x-y)
+# elif op == '/':
+#     print(x/y)
+# elif op == '*':
+#     print(x*y)
+
+# a = input("")
+
+# if a != "1, 2, 3, 4, 5, 6, 7, 8, 9, 0, *, /, +, -" :
+#     print(eval(input()))
+# else:
+#     print("error")
+
+#st = "hello world dear friend"
+#print(st.split(""))
+
+s = 'danchik'
+print(s[-2 :])
+
+
